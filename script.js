8fbb5481746268c839e4905c81221268af7b10ea
ammarkasbaoui17 <151763510+ammarkasbaoui17@users.noreply.github.com>
2024-10-21 15:09:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "Hello, I am Ammar Kasbaoui, a Computer Science Student specializing in Big Data & AI.";
+let index = 0;
+
+function typeWriter() {
+    if (index < text.length) {
+        document.getElementById('typewriter').innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, 100);  // Adjust typing speed here
+    }
+}
+
+window.onload = typeWriter;
