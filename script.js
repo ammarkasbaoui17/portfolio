f7c1b1853fad5561536127dfc30e5d15a53d7976
ammarkasbaoui17 <151763510+ammarkasbaoui17@users.noreply.github.com>
2024-10-22 22:45:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Typewriter effect for terminal
 const text = "Hello, I am Ammar Kasbaoui, a Computer Science Student specializing in Big Data & AI.";
 let index = 0;
 
@@ -8,5 +9,3 @@ function typeWriter() {
         setTimeout(typeWriter, 100);  // Adjust typing speed here
     }
 }
-
-window.onload = typeWriter;
